3e988d0781eb07b2ac125303a2c4d778af625d91
Aftr <evgenijzulin629@gmail.com>
2025-06-02 22:12:28 +0700

    Create execute.lua

---
+++ execute.lua
@@ -0,0 +1,3 @@
+-- There is a loader GUI. API Status of Script: https://api.aftrhub.xyz/APISTATUS.lua
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/AftrHub/refs/heads/main/loader.lua"))()
