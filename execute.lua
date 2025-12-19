efc5a0d32e07ad4e0eb68617ccbbd37203c3458d
dr. aftrskiy _ <evgenijzulin629@gmail.com>
2025-12-19 19:20:55 +0700

    Update execute.lua

--- execute.lua
+++ execute.lua
@@ -1,3 +1,2 @@
--- There is a loader GUI. API Status of Script: https://api.aftrhub.xyz/APISTATUS.lua
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/aftermath-dev/AftrHub/refs/heads/main/loader.lua"))()
+-- WAIT THE FUCKING NEWS, WE ARE PLANNING NEW HUB
+setclipboard("https://luau.xyz/link.html")
